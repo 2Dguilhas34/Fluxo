2ec2fb9752bf43f9c38d0958ddf50add2642c46e
2Dguilhas34 <00001121141328sp@al.educacao.sp.gov.br>
2024-09-18 10:33:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-Guilherme Borges da Silva Guimarães 2D
+Guilherme Borges da Silva Guimarães 2D 18-09-24
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
